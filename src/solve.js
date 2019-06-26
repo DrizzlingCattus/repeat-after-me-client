d879f5eeb00b150fbdc2d78212fe4245be9d505a
Yun Hwan Kim <hit0473@gmail.com>
2019-06-26 08:34:02 +0000

    src: solve filter and solveQuiz view working for simple quiz

--- src/solve.js
+++ src/solve.js
@@ -1,6 +1,17 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+import axios from 'axios';
 
+import { ENV, ROUTE } from './env.js';
+import { Home } from './home.js';
+import { SolveQuiz } from './solveQuiz.js';
+
+
+const QUIZ_TYPE = {
+    SIMPLE: "simple",
+    COMPLEX: "complex",
+    ALL: "all"
+};
 
 class Solve extends React.Component {
     constructor(props) {
@@ -10,10 +21,180 @@ class Solve extends React.Component {
     render() {
         return (
             <Router>
-                <div>
-                    <p> hello Solve! </p>
-                </div>
+                <Switch>
+                    <Route exact path={`${ROUTE.HOME}`} component={Home} />
+                    <Route exact path={`${ROUTE.SOLVE}`} component={SolveInner} />
+                    <Route exact path={`${ROUTE.SOLVE_QUIZ}`} component={SolveQuiz} />
+                </Switch>
             </Router>
+        );
+    }
+}
+
+class SolveInner extends React.Component {
+    constructor(props) {
+        super(props);
+        const curr = new Date();
+        const today = `${curr.getFullYear()}-${curr.getMonth()+1}-${curr.getDate()}`;
+        this.state = {
+            quizType: '',
+            startDate: today,
+            endDate: today,
+            isSubmitted: false,
+            searchResult: []
+        };
+
+        this.handleFilterQuizType = this.handleFilterQuizType.bind(this);
+        this.handleFilterDate = this.handleFilterDate.bind(this);
+        this.handleFilterKeyword = this.handleFilterKeyword.bind(this);
+        this.handleFilterSubmit = this.handleFilterSubmit.bind(this);
+    }
+
+    requestSimpleQuiz() {
+        let startDate = this.state.startDate;
+        let endDate = this.state.endDate;
+        const options = {
+            method: 'GET',
+            url: ENV.GET_SIMPLE_QUIZ_FROM_DATE_URL + `/${startDate}/${endDate}`,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            //TODO: add date for keyword searching
+        };
+        axios(options).then((res) => {
+            if(res.status == 200) {
+                const quizs = res.data.slice();
+                if(quizs.length === 0) {
+                    alert("search result empty");
+                } else {
+                    this.setState({
+                        isSubmitted: true,
+                        searchResult: quizs
+                    });
+                }
+            }
+        });
+    }
+
+    requestComplexQuiz() {
+        console.log("TODO: complex quiz request");
+    }
+
+    requestAllQuiz() {
+        console.log("TODO: all quiz request");
+    }
+
+    handleFilterQuizType(e) {
+        const type = e.target.value;
+        console.log("filter quiz type : " + type);
+        this.setState({
+            quizType: type
+        });
+    }
+
+    handleFilterDate(e) {
+        const next = {};
+        if(e.target.id == 'start-date') {
+            next.startDate = e.target.value;
+        } else if(e.target.id == 'end-date') {
+            next.endDate = e.target.value;
+        } else {
+            throw Error('date type is not either start or end');
+        }
+        this.setState(next);
+    }
+
+    handleFilterKeyword(e) {
+
+    }
+
+    handleFilterSubmit(e) {
+        e.preventDefault();
+        let type = this.state.quizType;
+        if(type == QUIZ_TYPE.SIMPLE) {
+            this.requestSimpleQuiz();
+        } else if(type == QUIZ_TYPE.COMPLEX) {
+            this.requestComplexQuiz();
+        } else if(type == QUIZ_TYPE.ALL) {
+            this.requestAllQuiz();
+        } else {
+            alert("Need to select quiz type. (simple, complex, all)");
+        }
+    }
+
+
+    render() {
+        if(this.state.isSubmitted) {
+            return (
+                <Redirect 
+                    from={`${ROUTE.SOLVE}`} 
+                    to={{
+                        pathname: `${ROUTE.SOLVE_QUIZ}`,
+                        state: {
+                            searchResult: this.state.searchResult.slice()
+                        }
+                    }} 
+                />
+            );
+        }
+
+        return(
+            <div>
+                <SolveStatus />
+                <QuizFilter 
+                    startDate={this.state.startDate}
+                    endDate={this.state.endDate}
+                    quizTypeHandler={this.handleFilterQuizType}
+                    dateHandler={this.handleFilterDate}
+                    keywordHandler={this.handleFilterKeyword}
+                    submitHandler={this.handleFilterSubmit} 
+                />
+            </div>
+        );
+    }
+}
+
+class SolveStatus extends React.Component {
+    render() {
+        return (
+            <div>
+                <p> solve status is calling! </p>
+            </div>
+        );
+    }
+}
+
+class QuizFilter extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <form id="quiz-filter-form" onSubmit={this.props.submitHandler}>
+                    <div>
+                        <input type="radio" name="quiz" value="simple" onChange={this.props.quizTypeHandler} /> Simple
+                        <input type="radio" name="quiz" value="complex" onChange={this.props.quizTypeHandler} /> Complex
+                        <input type="radio" name="quiz" value="all" onChange={this.props.quizTypeHandler} /> All
+                    </div>
+                    <div>
+                        <label> Start Date </label>
+                        <input id="start-date" type="date" value={this.props.startDate} onChange={this.props.dateHandler} required />
+                    </div>
+                    <div>
+                        <label> End Date </label>
+                        <input id="end-date" type="date" value={this.props.endDate} onChange={this.props.dateHandler} required />
+                    </div>
+                    <div>
+                        <label> TODO: input Keywords </label>
+                    </div>
+                    <div>
+                        <input type="submit" value="search" />
+                    </div>
+                </form>
+            </div>
         );
     }
 }
---
+++ src/solveQuiz.js
@@ -0,0 +1,266 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch, Redirect } from 'react-router-dom';
+
+import { ENV, ROUTE } from './env.js';
+import { Solve } from './solve.js';
+
+class SolveQuiz extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <Router>
+                <Switch>
+                    <Route exact path={`${ROUTE.SOLVE}`} component={Solve} />
+                    <Route 
+                        exact path={`${ROUTE.SOLVE_QUIZ}`} 
+                        render={
+                            (props) => {
+                                return <SolveQuizInner quizs={props.location.state.searchResult} />
+                            }
+                        }
+                    />
+                </Switch>
+            </Router>
+        );
+    }
+}
+
+class SolveQuizInner extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            lastIndex: this.props.quizs.length - 1,
+            // -1 is first quiz
+            currQuiz: this.getNextQuiz(-1),
+            isShowHints: false,
+            isShowAnswer: false,
+            isSolveEnd: false
+        };
+        this.handleHints = this.handleHints.bind(this);
+        this.handleAnswer = this.handleAnswer.bind(this);
+        this.handleNext = this.handleNext.bind(this);
+    }
+
+    getNextQuiz(currIndex) {
+        const nextIndex = currIndex + 1;
+        const next = currIndex < 0 ? this.props.quizs[0] : this.props.quizs[nextIndex];
+        return {
+            index: nextIndex,
+            date: next.date,
+            description: next.quiz,
+            answer: next.answer,
+            hints: next.hints.slice()
+        };
+    }
+
+    handleHints() {
+        this.setState({
+            isShowHints: true
+        });
+    }
+
+    handleAnswer() {
+        this.setState({
+            isShowAnswer: true
+        });
+    }
+
+    handleNext() {
+        if(this.state.currQuiz.index >= this.state.lastIndex) {
+            this.setState({
+                isSolveEnd: true
+            });
+        } else {
+            this.setState((prev) => {
+                return {
+                    currQuiz: this.getNextQuiz(prev.currQuiz.index),
+                    isShowAnswer: false,
+                    isShowHints: false
+                };
+            });
+        }
+    }
+
+    render() {
+        if(this.state.isSolveEnd) {
+            return <EndSolveQuiz />
+        }
+        return (
+            <div>
+                <div>
+                    <QuizDescription description={this.state.currQuiz.description} />
+                </div>
+                <div>
+                    <QuizHints hints={this.state.currQuiz.hints} isVisable={this.state.isShowHints} />
+                </div>
+                <div>
+                    <QuizAnswer answer={this.state.currQuiz.answer} isVisable={this.state.isShowAnswer} />
+                </div>
+                <div>
+                    <ShowHints showHintHandler={this.handleHints} isClicked={this.state.isShowHints} />
+                    <ShowAnswer showAnswerHandler={this.handleAnswer} isClicked={this.state.isShowAnswer} />
+                    <NextQuiz nextQuizHandler={this.handleNext} />
+                </div>
+            </div>
+        );
+    }
+}
+
+class EndSolveQuiz extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isEndClick: false
+        };
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick() {
+        this.setState({
+            isEndClick: true
+        });
+    }
+
+    render() {
+        if(this.state.isEndClick) {
+            return (
+                <Redirect from={`${ROUTE.SOLVE_QUIZ}`} to={`${ROUTE.SOLVE}`} />
+            );
+        }
+        return (
+            <div>
+                <p> End solving quiz! </p>
+                <button type="button" onClick={this.handleClick}>
+                    end
+                </button>
+            </div>
+        );
+    }
+}
+
+class QuizDescription extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <p> Quiz </p>
+                <p> {this.props.description} </p>
+            </div>
+        );
+    }
+}
+
+class QuizHints extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        if(!this.props.isVisable) {
+            return (
+                <div>
+                    <p> Hints </p>
+                    ...
+                </div>
+            );
+        }
+        const hintString = this.props.hints.length === 0 ? 'no hint' : this.props.hints.reduce((result, curr) => {
+            return `${result}, ${curr}`;
+        });
+        return (
+            <div>
+                <p> Hints </p>
+                <p> {hintString} </p>
+            </div>
+        );
+    }
+}
+
+class QuizAnswer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        if(!this.props.isVisable) {
+            return (
+                <div>
+                    <p> Answer </p>
+                    ...
+                </div>
+            );
+        }
+        return (
+            <div>
+                <p> Answer </p>
+                <p> {this.props.answer} </p>
+            </div>
+        );
+    }
+}
+
+class ShowHints extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <button 
+                    type="button" 
+                    autoComplete="off"
+                    disabled={this.props.isClicked}
+                    onClick={this.props.showHintHandler}>
+                    Show hints
+                </button>
+            </div>
+        );
+    }
+}
+
+class ShowAnswer extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <button
+                    type="button"
+                    autoComplete="off"
+                    disabled={this.props.isClicked}
+                    onClick={this.props.showAnswerHandler}>
+                    Show answer
+                </button>
+            </div>
+        );
+    }
+}
+
+class NextQuiz extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                <button
+                    type="button"
+                    onClick={this.props.nextQuizHandler}>
+                    Next
+                </button>
+            </div>
+        );
+    }
+}
+
+export { SolveQuiz };
